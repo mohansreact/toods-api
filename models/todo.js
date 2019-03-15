7b5d9cc84e30677267ddfe39cb04c158c5559f1c
mohankumar <mohankumar73@hotmail.com>
2019-03-15 05:19:36 +0000

    final commit

--- models/todo.js
+++ models/todo.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var todoSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: 'Name cannot be blank!'
+        required: 'Please fill it!'
     },
     completed: {
         type: Boolean,
